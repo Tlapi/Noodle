6ee5beef1696b59b003c88b0dbfd2e47224510ca
Jan Tlapak <jan@mfcc.cz>
2013-07-10 13:54:40 +0200

    db

--- data/noodle2.sql
+++ data/noodle2.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jul 03, 2013 at 06:44 AM
+-- Generation Time: Jul 10, 2013 at 11:53 AM
 -- Server version: 5.5.27
 -- PHP Version: 5.4.11
 
@@ -23,27 +23,69 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `filebank`
+--
+
+CREATE TABLE IF NOT EXISTS `filebank` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(250) COLLATE utf8_czech_ci NOT NULL,
+  `size` int(11) NOT NULL,
+  `mimetype` varchar(250) COLLATE utf8_czech_ci NOT NULL,
+  `isactive` int(11) NOT NULL,
+  `savepath` varchar(250) COLLATE utf8_czech_ci NOT NULL,
+  `keywords` varchar(500) COLLATE utf8_czech_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `filebank_keyword`
+--
+
+CREATE TABLE IF NOT EXISTS `filebank_keyword` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fileid` int(11) NOT NULL,
+  `value` varchar(250) COLLATE utf8_czech_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `modules`
 --
 
 CREATE TABLE IF NOT EXISTS `modules` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parent_row_id` int(11) DEFAULT NULL,
+  `parent_entity` varchar(255) COLLATE utf8_czech_ci DEFAULT NULL,
   `name` varchar(255) COLLATE utf8_czech_ci NOT NULL,
   `title` varchar(255) COLLATE utf8_czech_ci DEFAULT NULL,
   `select_id` int(11) NOT NULL,
+  `picture` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci AUTO_INCREMENT=16 ;
 
 --
 -- Dumping data for table `modules`
 --
 
-INSERT INTO `modules` (`id`, `name`, `title`, `select_id`) VALUES
-(2, 'asdf', 'asdf', 0),
-(3, 'test', '', 0),
-(4, 'asdf', 'asdf', 1),
-(5, 'sadf', '', 1),
-(6, 'asdf', '', 1);
+INSERT INTO `modules` (`id`, `parent_row_id`, `parent_entity`, `name`, `title`, `select_id`, `picture`) VALUES
+(2, NULL, NULL, 'asdf', 'asdf', 0, 0),
+(3, NULL, NULL, 'test', '', 0, 0),
+(4, NULL, NULL, 'asdf', 'asdf', 1, 0),
+(5, NULL, NULL, 'sadf', '', 1, 0),
+(6, NULL, NULL, 'asdf', '', 1, 0),
+(7, NULL, NULL, 'asd', 'asd', 3, 0),
+(8, NULL, NULL, 'asd', 'asd', 3, 0),
+(9, NULL, NULL, 'asd', 'asd', 3, 0),
+(10, NULL, NULL, 'asd', 'asd', 3, 0),
+(11, NULL, NULL, 'Three', 'asd', 3, 0),
+(12, NULL, NULL, 'Three', '', 3, 0),
+(13, NULL, NULL, 'Three', '', 3, 0),
+(14, NULL, NULL, 'asdf', 'asdf', 1, 0),
+(15, NULL, NULL, 'asdf', '', 3, NULL);
 
 -- --------------------------------------------------------
 
@@ -53,17 +95,24 @@ INSERT INTO `modules` (`id`, `name`, `title`, `select_id`) VALUES
 
 CREATE TABLE IF NOT EXISTS `relation` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parent_row_id` int(11) DEFAULT NULL,
+  `parent_entity` varchar(255) COLLATE utf8_czech_ci DEFAULT NULL,
   `title` varchar(255) COLLATE utf8_czech_ci NOT NULL,
   `description` text COLLATE utf8_czech_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci AUTO_INCREMENT=2 ;
+  PRIMARY KEY (`id`),
+  KEY `parent_row_id` (`parent_row_id`,`parent_entity`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci AUTO_INCREMENT=8 ;
 
 --
 -- Dumping data for table `relation`
 --
 
-INSERT INTO `relation` (`id`, `title`, `description`) VALUES
-(1, 'title', 'description');
+INSERT INTO `relation` (`id`, `parent_row_id`, `parent_entity`, `title`, `description`) VALUES
+(1, NULL, NULL, 'title', 'description'),
+(3, NULL, NULL, 'My another row', 'Description'),
+(5, 15, 'Modules\\Entity\\Test', 'asdfsadf', ''),
+(6, 15, 'Modules\\Entity\\Test', 'asdfsadf2', ''),
+(7, 15, 'Modules\\Entity\\Test', 'asadsa 9f87987', ' a6sd7f68 7adsf');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
